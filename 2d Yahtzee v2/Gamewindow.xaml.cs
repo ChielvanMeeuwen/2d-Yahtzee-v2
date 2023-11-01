4684bfb66a4f23054f67b6572f34aec09ea588c7
Chiel van Meeuwen <chiel.van.meeuwen@student.nhlstenden.com>
2023-11-01 18:36:23 +0100

    Aanpassing highscore image

--- 2d Yahtzee v2/Gamewindow.xaml.cs
+++ 2d Yahtzee v2/Gamewindow.xaml.cs
@@ -136,6 +136,7 @@ namespace _2d_Yahtzee_v2
                     if (rollCount < existingScore)
                     {                       
                         ((App)Application.Current).SharedDataStore.highScores[PlayerName] = rollCount;
+                        ((App)Application.Current).SharedDataStore.highscore = true;
                     }
                    
                 }
